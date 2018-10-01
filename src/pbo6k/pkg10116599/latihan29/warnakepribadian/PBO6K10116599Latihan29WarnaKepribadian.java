a288da3d75b97a9383e93d773dd5f7705895641e
Roby <robyeka@email.unikom.ac.id>
2018-10-01 22:11:43 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan29/warnakepribadian/PBO6K10116599Latihan29WarnaKepribadian.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan29.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program :
+ * 
+ */
+public class PBO6K10116599Latihan29WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
